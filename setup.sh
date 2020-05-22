d36bac4a612b9c393bd141d379a8aeb355674d55
ququzone <xueping.yang@gmail.com>
2020-05-23 00:30:57 +0800

    fix ckb environment string bug

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 set -ex
 
 if [ ! -f /bin/ckb ]; then
-  wget https://github.com/nervosnetwork/ckb/releases/download/v$CKB_VERSION/ckb_v$CKB_VERSION_x86_64-unknown-linux-gnu.tar.gz -O /tmp/ckb_v$CKB_VERSION_x86_64-unknown-linux-gnu.tar.gz
-  cd /tmp && tar xzf ckb_v$CKB_VERSION_x86_64-unknown-linux-gnu.tar.gz
-  mv /tmp/ckb_v$CKB_VERSION_x86_64-unknown-linux-gnu/ckb /bin/ckb
-  rm -rf /tmp/ckb_v$CKB_VERSION_x86_64-unknown-linux-gnu* 
+  wget https://github.com/nervosnetwork/ckb/releases/download/v"$CKB_VERSION"/ckb_v"$CKB_VERSION"_x86_64-unknown-linux-gnu.tar.gz -O /tmp/ckb_v"$CKB_VERSION"_x86_64-unknown-linux-gnu.tar.gz
+  cd /tmp && tar xzf ckb_v"$CKB_VERSION"_x86_64-unknown-linux-gnu.tar.gz
+  mv /tmp/ckb_v"$CKB_VERSION"_x86_64-unknown-linux-gnu/ckb /bin/ckb
+  rm -rf /tmp/ckb_v"$CKB_VERSION"_x86_64-unknown-linux-gnu* 
 fi
 
 [ -d /data/node ] || ckb init -c $NETWORK -C /data/node
